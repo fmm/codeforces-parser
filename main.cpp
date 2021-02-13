970f344688c8d7fc663b1b2b0e02f5086f1586df
Filipe Martins <filipe.martins@incognia.com>
2021-02-13 12:03:10 -0300

    updates template

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+#define dbg(x) cerr << #x << " == " << x << endl
+#define _ << " , " <<
+ 
+using namespace std;
+ 
+template<class T> void pv(T a, T b) { for (T i = a; i != b; ++i) cerr << *i << " "; cerr << endl; }
+
+const int maxn = 2e5 + 100;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+  int T=1;
+  cin>>T;
+  for(int t=0;t<T;t++){
+  }
+  cout.flush();
+  return 0;
+}
